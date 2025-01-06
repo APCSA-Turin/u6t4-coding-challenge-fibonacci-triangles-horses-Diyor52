f4bdc3ce064f21b3dd1681c9d3e9cd3fb1d6ab29
Diyor52 <diyora2@nycstudents.net>
2025-01-06 17:30:38 +0000

    Diyor challenge

--- src/main/java/com/example/project/Fibonacci.java
+++ src/main/java/com/example/project/Fibonacci.java
@@ -1,26 +1,28 @@
 package com.example.project;
 
 public class Fibonacci {
-  // instance variable
+  // Instance variable to store the Fibonacci sequence
   private int[] sequence;
 
   /** Constructor: sets sequence to an array containing the first seqLen numbers
       in a Fibonacci sequence. A Fibonacci sequence is formed by summing the
       previous two numbers to generate the next number,
       e.g. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34…
-
       All Fibonacci sequences start out with 0 and 1 as the first two numbers.
-
       PRECONDITION: seqLen >= 2
-    */
+  */
   public Fibonacci(int seqLen) {
-    /* implement me */
+    sequence = new int[seqLen];
+    sequence[0] = 0; 
+    sequence[1] = 1; 
+    for (int k = 2; k < seqLen; k++) {
+        sequence[k] = sequence[k - 1] + sequence[k - 2];
+    }
   }
 
-  /** Getter method: returns a reference to the sequence array
-    */
+  /** Getter method: returns a reference to the sequence array */
   public int[] getSequence() {
-    /* implement me */
+    return sequence; 
   }
 
   /** Returns the index in the array where a particular value, searchVal, is
@@ -29,26 +31,31 @@ public class Fibonacci {
       sequences longer than 2 numbers)
    */
   public int getIndexOf(int searchVal) {
-    /* implement me */
+    for (int i = 0; i < sequence.length; i++) {
+      if (sequence[i] == searchVal) {
+        return i;
+      }
+    }
+    return -1; 
   }
 
   /** Assigns sequence to a new array that extends the current sequence by
-      howManyMore numbers of the Fibonnaci sequence
-
-      For example, if sequence is {0, 1, 1, 2, 3, 5} and this method is called
-      with howManyMore = 3, sequence would be assigned to a new array with
-      the next 3 Fibonacci numbers added: {0, 1, 1, 2, 3, 5, 8, 13, 21}
-   */
+      howManyMore numbers of the Fibonacci sequence */
   public void extendBy(int howManyMore) {
-    /* implement this method */
+    int[] newArray = new int[sequence.length + howManyMore];
+    for (int i = 0; i < this.sequence.length; i++) {
+      newArray[i] = sequence[i];
+    }
+    
+    // Generate the new Fibonacci numbers and add them to the new array
+    for (int k = this.sequence.length; k < newArray.length; k++) {
+      newArray[k] = newArray[k - 1] + newArray[k - 2];
+    }
+    sequence = newArray;
   }
 
-  /** Returns a string that represents the sequence array nicely formatted, for
-   *  example, if sequence == [2, 3, 7], this method should return the String
-   *  "[2, 3, 7]"
-   *  USE THE ARRAYPRINTER UTILITY CLASS IN YOUR SOLUTION TO THIS METHOD
-   */
+  /** Returns a string that represents the sequence array nicely formatted */
   public String fibonacciString() {
-    /* implement this method using the utility class */
+    return ArrayPrinter.printableString(sequence);
   }
 }
--- src/main/java/com/example/project/Point.java
+++ src/main/java/com/example/project/Point.java
@@ -1,22 +1,37 @@
 package com.example.project;
 
+
 public class Point {
-    private int x;
-    private int y;
-    
-    public Point(int x, int y) {
+  private int x;
+  private int y;
+  
+  public Point(int x, int y) {
       this.x = x;
       this.y = y;
-    }
-    
-    // Returns distance between this Point object and another Point object
-    public double distanceTo(Point other) {
-      /* IMPLEMENT ME */
-    }
-  
-    // Returns a string in the format: (x, y)
-    public String pointInfo() {
-      /* IMPLEMENT ME */
-    }
   }
   
+ 
+  public int getX() {
+      return x;
+  }
+
+  public int getY() {
+      return y;
+  }
+  public void setX(int newx){
+     x= newx;
+  }
+  public void setY(int newy){
+    y= newy;
+ }
+
+  public double distanceTo(Point other) {
+      int diffx = x - other.getX();
+      int diffy = y - other.getY();
+      return Math.sqrt(diffx * diffx + diffy * diffy);
+  }
+
+  public String pointInfo() {
+      return "(" + getX() + ", " + getY() + ")";
+  }
+}
